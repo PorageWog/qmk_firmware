187dd7838e0b572b515aa2d7709157c5fce85cc7
Chris Walker <chris.walker@arria.com>
2022-02-07 09:00:25 +0000

    Reduced the default tapping term to 200.

--- keyboards/keebio/iris/keymaps/PorageWog/config.h
+++ keyboards/keebio/iris/keymaps/PorageWog/config.h
@@ -22,5 +22,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_LAYERS
 #define IGNORE_MOD_TAP_INTERRUPT
 #define COMBO_COUNT 1
-#define TAPPING_TERM 210
+#define TAPPING_TERM 200
 #define PERMISSIVE_HOLD

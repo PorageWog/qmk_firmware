f340ba2c42b52a10cd8ee4542a0949e49a59cb9f
Chris Walker <chris.walker@arria.com>
2023-04-26 09:11:31 +0100

    Added a SNIP shortcut.

--- keyboards/keebio/iris/keymaps/PorageMak-DH/keymap.c
+++ keyboards/keebio/iris/keymaps/PorageMak-DH/keymap.c
@@ -21,6 +21,7 @@
 #define FORMAT LCA(KC_L)
 #define HINT C(KC_P)
 #define VSMENU RCS(KC_P)
+#define SNIP SGUI(KC_S)
 
 #define KC_COLEMAK DF(_COLEMAK)
 #define KC_MISC MO(_QWERTY)
@@ -50,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_COLEMAK] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_QWERT,_______ ,_______ ,FIND    ,VSMENU  ,TEST                               ,RENAME  ,USAGES  ,_______ ,FORMAT  ,HINT    ,_______
+     KC_QWERT,_______ ,SNIP    ,FIND    ,VSMENU  ,TEST                               ,RENAME  ,USAGES  ,_______ ,FORMAT  ,HINT    ,_______
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
     ,KC_TAB  ,KC_Q    ,KC_W    ,KC_F    ,KC_P    ,KC_B                               ,KC_J    ,KC_L    ,KC_U    ,KC_Y    ,KC_SCLN ,KC_BSPC
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
